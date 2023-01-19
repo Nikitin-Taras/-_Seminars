324ee4dc6b1a10621313e421da709e83dd39d64b
Tarik <nikitin-taras@yandex.ru>
2023-01-19 20:12:26 +0200

    add Task 5 and Task7

---
+++ Task_5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показываеет все целые числа в промежутке от -N до N
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = -1 * N;
+
+while (current <= N)
+{
+    Console.Write($"{current}, ");
+    current++;
+}
+
+Console.Write("\b\b  ");
---
+++ Task_7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа
+// 456 -> 6
+// 782 -> 2
+
+// Console.WriteLine("Введите число: ");
+// int N = Convert.ToInt32(console.ReadLine());
+
+int num;
+do
+{
+    Console.Clear();
+    Console.Write("Введите число: ");
+    num = Convert.ToInt32(Console.ReadLine()); 
+} 
+while(num<100 || num>1000);
+Console.WriteLine($"result: {num%10}");
