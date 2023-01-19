d985ca3a1530bd71647e43fddb20ba92dac56687
Tarik <nikitin-taras@yandex.ru>
2023-01-19 12:08:01 +0200

    add Task 2

---
+++ Task1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите  программу которая на вход принимает 2 числа и проверяет, является ли первое число квадратом второго?
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int avg = num1*num1;
+
+if (avg == num2)
+{
+    Console.WriteLine("Первое число является квадратом второго ");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго ");
+};
--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя)
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = number*number;
+
+// Вывод в лоб
+Console.Write("Квадрат от ");
+Console.Write(number);
+Console.Write(" равен ");
+Console.WriteLine(sum);
+
+// Вывод через $
+Console.WriteLine($"Квадрат от {number} равен {sum}");
+
+// вывод с обращением к переменной
+Console.WriteLine("Квадрат от {0} равен {1}", number, sum);
+
+// Использованеи библиотеки Math
+int sum1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine("Квадрат от {0} равен {1}", number, sum1);
+
+// Вывод через +
+Console.WriteLine("Квадрат от " + number + " равен " + sum1);
